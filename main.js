103e06e80affa4940120002db5b1a0a6c2a70181
Alek Tysklind <91225228+outerpreneur@users.noreply.github.com>
2023-02-14 17:24:26 +0100

    arrow icons

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const menuDesktopItem = document.querySelectorAll("header nav ul li");
+const arrowIcon = document.querySelectorAll(".arrow");
 
 menuDesktopItem.forEach((link) => {
   link.addEventListener("click", () => {
+    console.log(link.childNodes[1]);
     link.nextElementSibling.classList.toggle("hidden");
-    console.log("work?");
+    link.childNodes[1].style.transform = "rotate(180deg)";
   });
 });
